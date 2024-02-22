a425ae8cc71c53ca87b404228d1e349c6333271b
QMblC <kurtovnevg@gmail.com>
2024-02-22 15:24:08 +0500

    orcs

--- Orcs.py
+++ Orcs.py
@@ -1,18 +1,21 @@
-orcs_heights = "1 1 2 2 2 2 1 1 1".split()
-grimmorhus_heights = "1 2 1".split()
 
-def get_different_height(array, height):
+orcs_heights = input().split()
+grimmorhus_heights = input().split()
+
+def get_different_height(height, array):
     different_heights = 0
     for i in array:
         if height != i:
             different_heights += 1
     return different_heights
 
+for height_index in range(len(grimmorhus_heights)):
 
-for grim_height in grimmorhus_heights:
-
+    grim_height = grimmorhus_heights[height_index]
+    
     same_height_left = 0
-    different_height_right = get_different_height(orcs_heights, grim_height)
+    different_height_right = get_different_height(grim_height, orcs_heights)
+    #different_height_right = len(orcs_heights) - orcs_heights.count(grim_height)
     points = 0
 
     for orc_height in orcs_heights:
@@ -20,7 +23,10 @@ for grim_height in grimmorhus_heights:
             same_height_left += 1
         else:
             different_height_right -= 1
-    
+        
         points = max(same_height_left * different_height_right, points)
 
-    print(points, end=' ')    
+    if (height_index == len(grimmorhus_heights) - 1):
+        print(points, end='') 
+    else:
+        print(points, end=' ')    
