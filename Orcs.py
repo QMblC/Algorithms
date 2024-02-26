495949efccc885204d3374ed81576a638d4eb43f
QMblC <kurtovnevg@gmail.com>
2024-02-26 11:21:23 +0500

    orcs

--- Orcs.py
+++ Orcs.py
@@ -1,11 +1,9 @@
-from functools import lru_cache
-
-
 orcs_heights = input().split()
 grimmorhus_heights = input().split()
 
+completed = dict()
+result = []
 
-@lru_cache
 def get_different_height(height, array = orcs_heights):
     different_heights = 0
     for i in array:
@@ -14,25 +12,28 @@ def get_different_height(height, array = orcs_heights):
     return different_heights
 
 for height_index in range(len(grimmorhus_heights)):
-    
+
+    if grimmorhus_heights[height_index] in completed:
+        result.append(str(completed[grimmorhus_heights[height_index]]))
+        continue   
+
     same_height_left = 0
     different_height_right = get_different_height(grimmorhus_heights[height_index])
-    #different_height_right = len(orcs_heights) - orcs_heights.count(grim_height)
+
     points = 0
 
     for orc_height in orcs_heights:
+
         if orc_height == grimmorhus_heights[height_index]:
             same_height_left += 1
             points = max(same_height_left * different_height_right, points)
-            
         else:
             different_height_right -= 1
 
         if (different_height_right < same_height_left):
             break
         
-
-    if (height_index == len(grimmorhus_heights) - 1):
-        print(points, end='') 
-    else:
-        print(points, end=' ')    
+    completed[grimmorhus_heights[height_index]] = points
+    result.append(str(points))
+ 
+print(" ".join(result))
