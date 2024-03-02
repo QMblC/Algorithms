27707a3ee5a68a4b3cb57a35c9799efbb15180e5
QMblC <kurtovnevg@gmail.com>
2024-03-02 23:57:23 +0500

    MonstrousPlanet

--- MonstrousPlanet.py
+++ MonstrousPlanet.py
@@ -1,55 +1,33 @@
 def split_string(string: str):
-    splitted_array = []
-    number = 0
-    count = 0
-    for char in string:
-        
-        if char == ' ':
-            splitted_array.append(int(round(number * 10 ** (count - 1))))
-            number = 0
-            count = 0
-        else:
-            number += int(char) * 0.1 ** count
-            count += 1
-            
-    splitted_array.append(int(round(number * 10 ** (count - 1))))
-    return splitted_array
+    return [int(x) for x in string.split()]
 
-def binary_search(first_type, second_type):
+def binary_search(first_type, second_type, l):
     left = 0
     right = l - 1
 
     while (right - left > 1):
-
         middle = (left + right) // 2
-
         if first_type[middle] <= int(second_type[middle]):
             left = middle
         else:
             right = middle
 
-    if (max(first_type[left], int(second_type[left])) < max(first_type[right], int(second_type[right]))):
+    if (max(first_type[left], second_type[left]) < max(first_type[right], second_type[right])):
         print(left)
     else:
         while right + 1 < l:
-            
             if (first_type[right] == first_type[right + 1]):
                 right += 1
             else:
                 break
-            
         print(right)
 
 n, m, l = split_string(input())
 
-first_type = [split_string(input()) for x in range(n)]
-second_type = [input().split() for x in range(m)]
+first_type = [split_string(input()) for i in range(n)]
+second_type = [split_string(input()) for i in range(m)]
 
 q = int(input())
-
-for x in range(q):
-
-    request = input().split()
-
-    binary_search(first_type[int(request[0])], second_type[int(request[1])])
-
+for i in range(q):
+    request = split_string(input())
+    binary_search(first_type[request[0]], second_type[request[1]], l)
