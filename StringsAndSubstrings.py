1d360bafc12db728082876652ad15186e1c19ed8
QMblC <kurtovnevg@gmail.com>
2024-03-12 15:06:55 +0500

    memoryLimit

--- StringsAndSubstrings.py
+++ StringsAndSubstrings.py
@@ -1,22 +1,35 @@
-string = input()
+def find_min_k(hash, powers, string):
+    max_sub_length = 0
+    for sub_length in range(1, len(string)):
+        if (len(string) % sub_length) != 0:
+            continue
+        for i in range(0, len(string) - sub_length * 2 + 1, sub_length):
+            f_r = hash[i + sub_length] - hash[i] * powers[sub_length]
+            s_r = hash[i + sub_length * 2] - hash[i + sub_length] * powers[sub_length]
+            if f_r != s_r:
+                continue
+            max_sub_length = max(max_sub_length, sub_length)
 
-n = len(string)
-last_k = 0
-last_substring = ""
+    if (max_sub_length != 0):
+        print(len(string) // max_sub_length, string[0: max_sub_length])
+    else:
+        print(1, string)
 
-for i in range(0, n):
-    if i == 0:
-        continue
-    if n % i != 0:
-        continue
-    substring = string[0 : i]
-    if substring * (n // i) == string:
-        last_k = n // i
-        last_substring = substring
-        break
+string = input() 
 
-if (last_k == 0):
-    last_k = 1
-    last_substring = string
+base = 26
+alphabet = [chr(x) for x in range(97, 97 + base)]
 
-print(last_k, last_substring)
+hash = [0] * (len(string) + 1)
+powers = [1] * (len(string) + 1)
+
+number = 1000000000003
+
+for i in range(len(string)):
+    hash[i + 1] = hash[i] * base + alphabet.index(string[i])
+    powers[i + 1] = powers[i] * base
+
+print(hash)
+print(powers)
+
+find_min_k(hash, powers, string)
