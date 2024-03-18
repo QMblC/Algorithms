c9ab0ca82200e648408beb4499afb96bc16f5ea4
QMblC <kurtovnevg@gmail.com>
2024-03-18 16:13:42 +0500

    MemoryLimit

--- Tiles.py
+++ Tiles.py
@@ -1,21 +1,28 @@
 n, m = [int(x) for x in input().split()]
 tiles = [int(x) for x in input().split()]
 
-middle = n // 2
-if n % 2 == 0:
-    middle += 1
+middle = n // 2 + 1
+
+result = []
+
+base = 1000000
+number = 1000000007
 
 real_hash = [0]
-reversed_hash = [0]
+
+pows = [1]
 
 for i in range(n):
-    real_hash.append(real_hash[i] * 1000000 + tiles[i])
-    reversed_hash.append(reversed_hash[i] * 1000000 + tiles[n - 1 - i])
+    real_hash.append((real_hash[i] * base + tiles[i]) % number)
+    pows.append((pows[i] * base) % number)
 
-print(real_hash)
-print(reversed_hash)
+for i in range(middle):
+    first_hash = (real_hash[i] - real_hash[0] * pows[i]) % number
+    second_hash = (real_hash[i * 2] - real_hash[i] * pows[i]) % number
 
+    print(first_hash, second_hash)
+    if str(first_hash) == str(second_hash)[::-1]:
+        result.append(str(n - i))
 
-print()
+print(" ".join(result))
 
-print(real_hash[3])
