a4df61949edc5555863be20956e2c6525265983a
QMblC <kurtovnevg@gmail.com>
2024-03-30 14:14:59 +0500

    TimeLimit

--- SelectionSort.py
+++ SelectionSort.py
@@ -1,12 +1,13 @@
-array = input().split()
+array = [int(x) for x in input().split()]
 
 for i in range(len(array)):
     index = i
     for j in range(i + 1, len(array)):
-        if int(array[j]) < int(array[index]):
+        if array[j] < array[index]:
             index = j
     array[i], array[index] = array[index], array[i]
 
     if i + 1 == len(array) // 2:
-        print(" ".join(array))
-print(" ".join(array))
+        print(*array, sep= " ")
+
+print(*array, sep= " ")
