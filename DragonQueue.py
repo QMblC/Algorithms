aa8c21cb868544d8254b5e8042cf290e57c82e8b
QMblC <kurtovnevg@gmail.com>
2024-04-08 23:08:37 +0500

    IDK

--- DragonQueue.py
+++ DragonQueue.py
@@ -1,31 +1,14 @@
-import re
 
-class DragonQueue:
-    def __init__(self) -> None:
-        self.body = []
-
-    def push_last(self, value):
-        self.body.append(value)
-
-    def push_middle(self, value):
-        middle = len(self.body) // 2 + len(self.body) % 2
-        self.body.insert(middle, value)
-
-    def push_first(self, value):
-        self.body.insert(0, value)
-
-    def pop(self):
-        return self.body.pop(0)
-
-queue = DragonQueue()
+queue = []
 
 for i in range(int(input())):
-    request = input()
-    if re.match("\+", request):
-        queue.push_last(request.split()[1])
-    elif re.match("\*", request):
-        queue.push_middle(request.split()[1])
-    elif re.match("\!", request):
-        queue.push_first(request.split()[1])
-    elif re.match("\-", request):
-        print(queue.pop())
+    request = input().split()
+    if "+" == request[0]:
+        queue.append(int(request[1]))
+    elif "*" == request[0]:
+        middle = len(queue) // 2 + len(queue) % 2
+        queue.insert(middle, int(request[1]))
+    elif "!" == request[0]:
+        queue.insert(0, int(request[1]))
+    elif "-" ==  request[0]:
+        print(queue.pop(0))
