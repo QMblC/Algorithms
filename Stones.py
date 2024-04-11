6d453a2b0b4f7b52c90d5ca99632723786049741
QMblC <kurtovnevg@gmail.com>
2024-04-11 13:52:33 +0500

    Stones_TL

--- Stones.py
+++ Stones.py
@@ -1,21 +1,23 @@
-stones = []
-
-def count_pairs(i: int, j: int):
-    if i >= stones_number:
+def count_pairs(i, j):
+    if j == stones_number:
         return 0
-    
-    if j >= stones_number:
-        return count_pairs(i + 1, i + 2)
-
-    if i < j and stones[i] >= stones[j]:
+    elif stones[i] >= stones[j]:
         return 1 + count_pairs(i, j + 1)
     else:
         return count_pairs(i, j + 1)
 
+def sum_pairs(i = 0):
+    if i == stones_number:
+        return 0
+    else:
+        return count_pairs(i, i + 1) + sum_pairs(i + 1)
+
+def handle():
+    for i in range(stones_number):
+        stones.append(int(input()))
+
 stones_number = int(input())
+stones = []
 
-for i in range(stones_number):
-    stones.append(int(input()))
-
-a = count_pairs(0, 1)
-print(a)
+handle()
+print(sum_pairs())
