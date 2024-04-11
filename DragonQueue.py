f63f8278d3f18eadf34ec655d5a42360dd726b05
QMblC <kurtovnevg@gmail.com>
2024-04-11 10:26:07 +0500

    DragonQueue_Done

--- DragonQueue.py
+++ DragonQueue.py
@@ -1,14 +1,34 @@
 
-queue = []
+class DragonQueue:
+    def __init__(self) -> None:
+        self.body = []
 
-for i in range(int(input())):
-    request = input().split()
-    if "+" == request[0]:
-        queue.append(int(request[1]))
-    elif "*" == request[0]:
-        middle = len(queue) // 2 + len(queue) % 2
-        queue.insert(middle, int(request[1]))
-    elif "!" == request[0]:
-        queue.insert(0, int(request[1]))
-    elif "-" ==  request[0]:
-        print(queue.pop(0))
+    def push_last(self, value):
+        self.body.append(value)
+
+    def push_middle(self, value):
+        middle = len(self.body) // 2 + len(self.body) % 2
+        self.body.insert(middle, value)
+
+    def push_first(self, value):
+        self.body.insert(0, value)
+
+    def pop(self):
+        return self.body.pop(0)
+
+
+def handle(queue):
+    for i in range(int(input())):
+        request = input().split()
+        if "+" == request[0]:
+            queue.push_last(request[1])
+        elif "*" == request[0]:
+            queue.push_middle(request[1])
+        elif "!" == request[0]:
+            queue.push_first(request[1])
+        elif "-" ==  request[0]:
+            print(queue.pop())
+
+queue = DragonQueue()
+
+handle(queue)
