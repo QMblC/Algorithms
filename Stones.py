1cb455c8011140b57c2eb6214972acf43e814c1e
QMblC <kurtovnevg@gmail.com>
2024-04-18 10:31:12 +0500

    IDK

--- Stones.py
+++ Stones.py
@@ -1,20 +1,21 @@
-def count_pairs(i, j):
-    if j == stones_number:
-        return 0
-    elif stones[i] >= stones[j]:
-        return 1 + count_pairs(i, j + 1)
-    else:
-        return count_pairs(i, j + 1)
+def count_pairs(i):
+    pairs = 0
+    for j in range(i + 1, stones_number):
+        if i < j and stones[i] >= stones[j]:
+            pairs += 1
+            pass
+    return pairs
 
 def sum_pairs(i = 0):
     if i == stones_number:
         return 0
     else:
-        return count_pairs(i, i + 1) + sum_pairs(i + 1)
+        return count_pairs(i) + sum_pairs(i + 1)
 
 def handle():
-    for i in range(stones_number):
+    for n in range(stones_number):
         stones.append(int(input()))
+
 
 stones_number = int(input())
 stones = []
