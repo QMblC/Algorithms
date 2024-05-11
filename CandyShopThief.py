fd3075b6845f1cbbf99ecb7cf0e57e4ac457cd37
QMblC <kurtovnevg@gmail.com>
2024-05-11 10:04:27 +0500

    CandyShop_Done

--- CandyShopThief.py
+++ CandyShopThief.py
@@ -1,5 +1,4 @@
 from typing import List
-import math
 
 class Cake:
     def __init__(self, price: int, volume: int) -> None:
@@ -11,54 +10,35 @@ class Cake:
         return self.ratio < other.ratio
 
 class ThiefBag:
-    def __init__(self, volume) -> None:
+    def __init__(self, volume: int) -> None:
+        self.volume = volume
         self.cost = 0
-        self.start_volume = volume
-        self.volume_left = volume
 
-    def add_cake(self, cake: Cake):
-        self.cost += cake.price
-        self.volume_left -= cake.volume
-
-    def add_cake_part(self, cake: Cake):
-        cake_part_ratio = self.volume_left / cake.volume
-        self.cost += cake.price * cake_part_ratio
-        self.volume_left -= cake.volume * cake_part_ratio
-    
-
-
-def get_sorted_data(n):
-    cakes = []
-    for i in range(n):
-
-        price, volume = [int(x) for x in input().split()]
-        cake = Cake(price, volume)
-        cakes.append(cake)
-
-    a = sorted(cakes, reverse=True)
-    return a
-
-def fill_thief_bag(cakes: List[Cake]):
-    thief_bag = ThiefBag(w)
-    for cake in cakes:
-        if thief_bag.volume_left - cake.volume == 0:
-            thief_bag.add_cake(cake)
-            break
-        elif thief_bag.volume_left - cake.volume < 0:
-            thief_bag.add_cake_part(cake)
-            break
+    def add(self, cake: Cake):
+        if self.volume >= cake.volume:
+            self.cost += cake.price
+            self.volume -= cake.volume
         else:
-            thief_bag.add_cake(cake)
-    return thief_bag
-n, w  = [int(x) for x in input().split()]
+            ratio = self.volume / cake.volume
+            self.cost += cake.price * ratio
+            self.volume = 0
 
-cakes = get_sorted_data(n)
+def get_maximum_value(cakes: List[Cake], capacity: int):
+    cakes.sort(reverse=True)
 
-cost = str(round(fill_thief_bag(cakes).cost,2))
+    thief_bag = ThiefBag(capacity)
+    for cake in cakes:
+        if thief_bag.volume == 0:
+            break
+        thief_bag.add(cake)
+    return thief_bag.cost
 
-if cost[-2] == '.':
-    print(cost + '0')
-elif not('.' in cost):
-    print(cost + ".00")
-else:
-    print(cost)
+n, w = [int(x) for x in input().split()]
+cakes = []
+
+for i in range(n):
+    price, volume = map(int, input().split())
+    cakes.append(Cake(price, volume))
+
+max_val = get_maximum_value(cakes, w)
+print(format(max_val, ".2f"))
