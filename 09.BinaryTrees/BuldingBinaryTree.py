b7d9e6362afeb26d05d06d764a1d0be682cc9ae4
QMblC <kurtovnevg@gmail.com>
2024-05-13 13:43:45 +0500

    BBT_Done

--- 09.BinaryTrees/BuldingBinaryTree.py
+++ 09.BinaryTrees/BuldingBinaryTree.py
@@ -7,46 +7,49 @@ class Root:
         self.left_branch = left_branch
         self.right_branch = right_branch
 
-    
+class Tree:
+    def __init__(self, array: List[int]) -> None:
+        self.root = self.create_tree(array, 0, len(array))
 
-def create_tree(array: List[int], left: int, right: int, depth: int):
+    def create_tree(self, array: List[int], left: int, right: int, depth = 0):
 
-    if right - left < 1:
-        return None
-    
-    if right - left == 1:
-        return Root(array[left], depth)
-    
-    middle = (left + right - 1) // 2
+        if right - left < 1:
+            return None
+        
+        if right - left == 1:
+            return Root(array[left], depth)
+        
+        middle = (left + right - 1) // 2
 
-    root = Root(array[middle], depth)
-    root.left_branch = create_tree(array, left, middle, depth + 1)
-    root.right_branch = create_tree(array, middle + 1, right, depth + 1)
+        root = Root(array[middle], depth)
+        root.left_branch = self.create_tree(array, left, middle, depth + 1)
+        root.right_branch = self.create_tree(array, middle + 1, right, depth + 1)
 
-    return root
+        return root
 
-def draw(root: Root, is_single_branch = True, string = ""):
+    @staticmethod
+    def draw(root: Root, is_single_branch = True, string = ""):
 
-    if root == None:
-        return
+        if root == None:
+            return
 
-    if root.depth == 0:
-        print(root.value)     
-        new_string = string + ""
-    
-    elif is_single_branch:
-        print(f"{string}└───{root.value}")
-        new_string = string + "    "
+        if root.depth == 0:
+            print(root.value)     
+            new_string = string + ""
+        
+        elif is_single_branch:
+            print(f"{string}└───{root.value}")
+            new_string = string + "    "
 
-    else:
-        print(f"{string}├───{root.value}")
-        new_string = string + "│   "
+        else:
+            print(f"{string}├───{root.value}")
+            new_string = string + "│   "
 
-    draw(root.left_branch, False, new_string)
-    draw(root.right_branch, True, new_string)
+        Tree.draw(root.left_branch, False, new_string)
+        Tree.draw(root.right_branch, True, new_string)
 
 
 array = [int(x) for x in input().split()]
 
-tree = create_tree(array, 0, len(array), 0)
-draw(tree)
+tree = Tree(array)
+Tree.draw(tree.root)
