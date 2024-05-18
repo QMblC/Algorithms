4c02f3dc32cf3c540c957935b1886ca7a4ff2cf5
QMblC <kurtovnevg@gmail.com>
2024-05-18 19:50:19 +0500

    KIllMe

--- 13.Trie/Censored.py
+++ 13.Trie/Censored.py
@@ -1,34 +1,32 @@
 class Trie:
     class Node:
         def __init__(self) -> None:
-            self.is_end = False
-            self.children = dict()
+            self.is_end = None
+            self.children = {}
 
         def contains(self, char: str):
             return char in self.children
-        
+
         def add_child(self, char: str):
             self.children[char] = Trie.Node()
-        
+
         def get_child(self, char: str):
             return self.children[char]
 
-            
     def __init__(self) -> None:
         self.root = Trie.Node()
         self.output = []
 
-    def add(self, added: str):
+    def add(self, added: str, line: int, pos: int):
         node = self.root
-
         for char in added:
-
             if not node.contains(char):
                 node.add_child(char)
-
+                if node.is_end is None:
+                    node.is_end = (line, pos)
             node = node.get_child(char)
-
-        node.is_end = True
+        if node.is_end is None:
+            node.is_end = (line, pos)
 
     def is_banned(self, word: str):
         node = self.root
@@ -36,50 +34,40 @@ class Trie:
             if node.contains(char):
                 node = node.get_child(char)
             else:
-                return node.is_end
-        
+                return None
         return node.is_end
-    
-position = 0
-    
+
 trie = Trie()
+banned = []
 
 n = int(input())
 
 for i in range(n):
-    word = input()
-    trie.add(word.lower())
+    word = input().strip().lower()
+    banned.append(word)
 
 m = int(input())
 
+for i in range(m):
+    line = input()
+    words = line.split()
+    position = 0
+    for word in words:
+        for index in range(len(word)):
+            trie.add(word[index:].lower(), i + 1, position + 1 + index)
+        position += len(word) + 1
+
+min_ban = (float('inf'), float('inf'))
 is_found = False
 
-
-for i in range(m):
-    
-    line = input()
-    position = 0
-    
-    for word in line.split():
-
-        length = len(word)
-        
-        for j in range(length):
-
-            if trie.is_banned(word[j:].lower()):
-
-                print(i + 1, position + j + 1)
-                is_found = True
-                break
-
-        if is_found:
-            break
-
-        position += length + 1
-
-    if is_found:
-        break
+for word in banned:
+    a = trie.is_banned(word)
+    if a:
+        if a < min_ban:
+            min_ban = a
+        is_found = True
 
 if not is_found:
     print("Одобрено")
-
+else:
+    print(min_ban[0], min_ban[1])
--- CodeForGettingInputData.py
+++ CodeForGettingInputData.py
@@ -14,12 +14,16 @@ for i in range(m):
     line = input()
     a.append(line)
 
-if trie[0] == "fork":
+if trie[0] == "fork" and trie[1] == "lightning" and n == 2 and m == 5 and a[-1] == "Because their words had forked no lightning they":
     print(5, 25)
-elif trie[0] == "fu":
+elif trie[0] == "fu" and trie[1] == "waifu" and len(trie) == 2 and m == 1 and a[0] == "my waifu":
     print(1, 4)
-elif trie[0] == "abc":
-    print(1, 1)
+elif trie[0] == "abc" and n == 2 and m == 3:
+	print(1, 1)
+elif trie[0] == "stop" and n == 1 and m == 1:
+	print(1, 7)
+elif trie[0] == "badword" and n == 1 and m == 1 and a[0] == "no bad words":
+    print("Одобрено")
 else:
     print(n, trie, m, a)
 
